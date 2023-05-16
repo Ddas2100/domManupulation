afd5507dc54819d93f62630591cadbfc2c15a624
Ddas2100 <ddas2100@gmail.com>
2023-05-16 23:25:51 +0530

    task5_Script

---
+++ task5_Script.js
@@ -0,0 +1,16 @@
+// GETELEMENTBYCLASSNAME //
+
+// var items = document.getElementsByClassName('list-group-item');
+// for(var i=0; i<items.length; i++) {
+//     items[i].style.backgroundColor='#f4f4f4';
+//     items[i].style.fontWeight= 'bold';
+// }
+
+// GETELEMENTBYTAGNAME //
+
+var li = document.getElementsByTagName('li');
+for(var i=0; i<li.length; i++) {
+    li[i].style.backgroundColor='#f4f4f4';
+    li[i].style.fontWeight= 'bold';
+}
+
